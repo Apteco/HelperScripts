f918bb7963590e456175dbc357c2e348e94c8e1e
Florian <florian.von.bracht@apteco.de>
2020-06-09 15:32:34 +0200

    Deploying a newer version of Get-Unixtime
    For usage in Hubspot scripts

---
+++ functions/Time/Get-Unixtime.ps1
@@ -0,0 +1,19 @@
+﻿# current unixtimestamp with the optional milliseconds
+Function Get-Unixtime {
+    
+    param(
+         [Parameter(Mandatory=$false)][switch] $inMilliseconds = $false
+        ,[Parameter(Mandatory=$false)][DateTime] $timestamp = ( Get-Date )
+    )
+
+    $multiplier = 1
+
+    if ( $inMilliseconds ) {
+        $multiplier = 1000
+    }
+
+    [long]$unixtime = [double]::Parse((Get-Date ($timestamp).ToUniversalTime() -UFormat %s)) * $multiplier
+
+   return $unixtime 
+
+}
