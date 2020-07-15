0917b8760984a0741d25e5de4a7b7da1e44f6db6
Florian <florian.von.bracht@apteco.de>
2020-07-15 17:02:15 +0200

    Create sqlserver.ps1
    First version to make it easier querying sqlserver

---
+++ functions/Database/sqlserver.ps1
@@ -0,0 +1,60 @@
+﻿
+
+Function Invoke-SqlServer {
+
+    param(
+         [Parameter(Mandatory=$true)][String]$instance
+        ,[Parameter(Mandatory=$true)][String]$database
+        ,[Parameter(Mandatory=$true)][String[]]$query
+        ,[Parameter(Mandatory=$false)][Switch]$executeScalar = $false
+    )
+
+    $connectionString = "Data Source=$( $instance );Initial Catalog=$( $database );Trusted_Connection=True;Connect Timeout=2400;"
+
+    # build connection
+    $connection = New-Object "System.Data.SqlClient.SqlConnection"
+    $connection.ConnectionString = $connectionString
+    $connection.Open()
+   
+   
+                  
+    try {
+
+        # execute command
+        $command = $connection.CreateCommand()
+        $command.CommandText = $query
+        
+        If ( $executeScalar ) {
+            $result = $command.ExecuteScalar()
+        } else {
+             # create datatable
+            $result = new-object "System.Data.DataTable"
+            $sqlResult = $command.ExecuteReader()
+            # load data
+            $result.Load($sqlResult, [System.Data.Loadoption]::Upsert)
+        }
+
+        
+    
+        
+           
+
+    } catch [System.Exception] {
+
+        $errText = $_.Exception
+        $errText | Write-Output
+        Write-Log "Error: $( $errText )"
+
+    } finally {
+    
+        # close connection
+        $connection.Close() 
+
+        
+
+    }
+
+    
+    return $result
+
+}
