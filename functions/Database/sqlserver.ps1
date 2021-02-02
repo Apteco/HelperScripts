fde26164a3b5c6bed8ea2ddb50504814342b64f2
FlorianSf <florian.von.bracht@apteco.de>
2021-02-02 01:40:38 +0100

    Update sqlserver.ps1
    Added the possibility to use credentials, not only trusted connection

--- functions/Database/sqlserver.ps1
+++ functions/Database/sqlserver.ps1
@@ -1,20 +1,59 @@
 
+<#
 
+It can be called like:
+
+$statementParams = @{
+    "query" = $insertStatement
+    "instance" = $settings.upload.databaseInstance
+    "database" = $settings.upload.databaseName
+    "executeNonQuery" = $true
+}
+
+if ( $settings.upload.trustedConnection ) {
+
+    $statementParams += @{
+        "trustedConnection" = $true
+    }
+
+} else {
+
+    $securePass = ConvertTo-SecureString ( Get-SecureToPlaintext $settings.upload.databasePass ) -AsPlainText -Force
+    $securePass.MakeReadOnly()
+    $sqlcred = New-Object -TypeName System.Data.SqlClient.SqlCredential -ArgumentList $settings.upload.databaseUser, $securePass
+    $statementParams += @{
+        "sqlCredential" = $sqlCred
+    }
+
+}
+
+$writtenRecords += Invoke-SqlServer @statementParams
+
+#>
 Function Invoke-SqlServer {
 
     param(
          [Parameter(Mandatory=$true)][String]$instance        
         ,[Parameter(Mandatory=$true)][AllowEmptyString()][String[]]$query
         ,[Parameter(Mandatory=$false)][String]$database = "master"
+        ,[Parameter(Mandatory=$false)][System.Data.SqlClient.SqlCredential]$sqlCredential = $null
+        ,[Parameter(Mandatory=$false)][Switch]$trustedConnection = $false
         ,[Parameter(Mandatory=$false)][Switch]$executeScalar = $false # Returns the first column in the first row
         ,[Parameter(Mandatory=$false)][Switch]$executeNonQuery = $false # Return the number of affected records (used for inserts, updates, deletes)
     )
 
-    $connectionString = "Data Source=$( $instance );Initial Catalog=$( $database );Trusted_Connection=True;Connect Timeout=2400;"
+#    $connectionString = "Data Source=$( $instance );Initial Catalog=$( $database );Trusted_Connection=True;Connect Timeout=2400;"
+    $connectionString = "Data Source=$( $instance );Initial Catalog=$( $database );Connect Timeout=2400"
+    If ( $trustedConnection ) {
+        $connectionString += ";Trusted_Connection=True"
+    }
 
     # build connection
     $connection = New-Object "System.Data.SqlClient.SqlConnection"
     $connection.ConnectionString = $connectionString
+    if ( $trustedConnection -eq $false ) {
+        $connection.Credential = $sqlCredential
+    }
     $connection.Open()
    
    
