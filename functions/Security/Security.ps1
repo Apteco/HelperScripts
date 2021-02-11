e4b8c1e2aaea038f7a09a9b9af64de526df521cc
FlorianSf <florian.von.bracht@apteco.de>
2021-02-12 00:21:28 +0100

    Changed the security script name

--- functions/Security/Security.ps1
+++ functions/Security/Security.ps1
@@ -1,13 +1,12 @@
 ﻿Function Get-PlaintextToSecure {
 
     param(
-         [Parameter(Mandatory=$true)][String]$String,
-         [Parameter(Mandatory=$false)][string]$keyFile = "$( $settings.aesFile )"
+         [Parameter(Mandatory=$true)][String]$String
     )
     
     # generate salt
-    Create-KeyFile -keyfilename "$( $keyFile )" -byteLength 32
-    $salt = Get-Content -Path "$( $keyFile )" -Encoding UTF8
+    Create-KeyFile -keyfilename "$( $scriptPath )\aes.key" -byteLength 32
+    $salt = Get-Content -Path "$( $scriptPath )\aes.key" -Encoding UTF8
 
     # convert
     $stringSecure = ConvertTo-secureString -String $String -asplaintext -force
@@ -25,7 +24,7 @@ Function Get-SecureToPlaintext {
     )
 
     # generate salt
-    $salt = Get-Content -Path "$( $settings.aesFile )" -Encoding UTF8
+    $salt = Get-Content -Path "$( $scriptPath )\aes.key" -Encoding UTF8
 
     #convert 
     $stringSecure = ConvertTo-SecureString -String $String -Key $salt
@@ -53,4 +52,26 @@ Function Create-KeyFile {
     }
     
     
+}
+
+<#
+Password encryption for Apteco Orbit
+#>
+Function Crypt-Password {
+
+    param(
+        [String]$password
+    )
+
+    $cryptedPassword = @()
+    $password.ToCharArray() | %{[int][char]$_} | ForEach {    
+        If ($_ % 2 -eq 0) {
+            $cryptedPassword += [char]( $_ + 1 )
+        } else {
+            $cryptedPassword += [char]( $_ - 1 )
+        } 
+    }
+
+    $cryptedPassword -join ""
+
 }
