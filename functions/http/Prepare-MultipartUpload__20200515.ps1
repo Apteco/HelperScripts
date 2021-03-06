56929df84207e18a96eab9caf42f8bf0552723b9
FlorianSf <florian.von.bracht@apteco.de>
2021-03-06 10:46:43 +0100

    Moved and improved the multipart/upload preparation function

---
+++ functions/http/Prepare-MultipartUpload__20200515.ps1
@@ -0,0 +1,57 @@
+
+
+Function Prepare-MultipartUpload {
+    param(
+        [Parameter(Mandatory=$true)][String]$path,
+        [Parameter(Mandatory=$false)]$part = $false
+    )
+
+    # standard settings
+    $uploadEncoding = "ISO-8859-1" #"UTF-8"
+    $crlf = "`r`n";
+    
+    # if multipart, remove the part prefix
+    $fileItem = Get-Item -Path $path
+    if ($part) {
+        $fileName = $fileItem.Name.Substring(0, $fileItem.Name.lastIndexOf('.')) 
+    } else {
+        $fileName = $fileItem.Name
+    }
+    
+    # get file, load and encode it
+    $fileBytes = [System.IO.File]::ReadAllBytes($fileItem.FullName)
+    $fileEncoded = [System.Text.Encoding]::GetEncoding($uploadEncoding).GetString($fileBytes)
+
+    # create guid for multipart upload
+    $boundary = [System.Guid]::NewGuid().ToString().replace("-","").PadLeft(57,"-")
+
+    # Get the mime type
+    $mimeType = [System.Web.MimeMapping]::GetMimeMapping($fileName)
+
+    # create body
+    $body = (
+        #"multipart/form-data; boundary=$( $boundary )",
+        "--$( $boundary )",
+        "Content-Disposition: form-data; name=`"file`"; filename=`"$( $fileName )`"",
+        "Content-Type: $( $mimeType )$( $crlf )",
+        #"Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet$( $crlf )",
+        $fileEncoded,
+        "--$( $boundary )--$( $crlf )" 
+    ) -join $crlf
+
+    # put it together
+    @{
+        "body"=$body
+        "contentType"="multipart/form-data; boundary=$( $boundary )"
+    }
+
+}
+
+<#
+#$headers.accept = "application/json, text/plain, */*"
+$part = Prepare-MultipartUpload -path "D:\Scripts\TriggerDialog\v2\swagger\Unbenannt1.xlsx"
+
+Invoke-RestMethod -Method Post -Uri "$( $settings.base )/testdata?customerId=$( $customerId )&campaignId=$( $campaignId )" -Headers $headers -ContentType $part.contentType -Verbose -Body $part.body #-TransferEncoding "Deflate"
+
+#$part.body | set-content -Path ".\text.txt" -Encoding UTF8
+#>
