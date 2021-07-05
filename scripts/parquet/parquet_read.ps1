98dcb2e96f83d8b53854a980dcd25697732c0e38
FlorianSf <florian.von.bracht@apteco.de>
2021-07-05 18:52:10 +0200

    Update parquet_read.ps1
    Next version of reading parquet data, next level is the usage of the row oriented api

--- scripts/parquet/parquet_read.ps1
+++ scripts/parquet/parquet_read.ps1
@@ -64,85 +64,113 @@ $chosenSchema = $schemas #| Out-GridView -PassThru
 
 "Reading $( $chosenSchema.Count ) schemas now"
 
+# Go through the schema items aka. row groups
 $chosenSchema | ForEach {
 
     $schemaMeta = $_
-
     "Reading $( $schemaMeta.Name )"
-
     $rowGroupReader = $reader.RowGroup($schemaMeta.Index)
 
-    $v = [System.Collections.ArrayList]@()
-    For ( $c = 2 ; $c -lt 3 ; $c++ ) { # ++$c or $c++ ? #$meta.NumColumns
+    # Prepare reading the data in batches
+    $rowsCount = $rowGroupReader.MetaData.NumRows
+    $batchSize = 101
+    $batches = [math]::Ceiling($rowsCount / $batchSize)
+    $colValues = [System.Collections.ArrayList]@()
+    $colNames = [System.Collections.ArrayList]@()
+    #$colMemory = [Hashtable]@{}
 
-        $columnReader = $rowGroupReader.Column($c)
-        
-        # Return the column description
-        <#
-        
-        ColumnOrder        : Undefined
-        LogicalType        : String
-        MaxDefinitionLevel : 1
-        MaxRepetitionLevel : 0
-        Name               : first_name
-        Path               : first_name
-        SchemaNode         : ParquetSharp.Schema.PrimitiveNode
-        PhysicalType       : ByteArray
-        SortOrder          : Unsigned
-        TypeLength         : 0
-        TypePrecision      : 0
-        TypeScale          : 0
-        
-        #>
-
-        $columnDescriptor = $columnReader.ColumnDescriptor
-
-        "Column $( $c ) - $( $columnDescriptor.Name )"
-
-        # Return the metadata of a column chunk
-        <#
-        
-        Compression           : Uncompressed
-        CryptoMetadata        :
-        Encodings             : {BitPacked, PlainDictionary, Rle}
-        FileOffset            : 17317
-        IsStatsSet            : False
-        NumValues             : 1000
-        TotalCompressedSize   : 2988
-        TotalUncompressedSize : 2988
-        Statistics            :
-        Type                  : ByteArray
-        
-        #>
-
-        # Prepare reading the data in batches
-        $colChunkMetaData = $rowGroupReader.MetaData.GetColumnChunkMetaData($c)
-        $rowsCount = $colChunkMetaData.NumValues
-        $batchSize = 101
-        $batches = [math]::Ceiling($rowsCount / $batchSize)
-        #$columnReader.Skip(10000) # skip x rows
-        #$columnReader.LogicalReader().BufferLength
-        #$columnReader.LogicalReader().LogicalType
-        #$columnReader.LogicalReader().HasNext
-        $logicalReader = $columnReader.LogicalReader()
-
-        # Read the data in batches
-        For ( $i = 0 ; $i -lt $batches ; $i++ ) {
-            # In the last batch call with the exact number of remaining rows
-            if ( $i -eq $batches - 1 ) {
-                $batchSize = $rowsCount % ( $i * $batchSize)
-            }
-            # Call always the next n rows
-            $v.AddRange($logicalReader.ReadAll($batchSize))
+    For ( $i = 0 ; $i -lt 2 ; $i++ ) { # batches
+        # In the last batch call with the exact number of remaining rows
+        if ( $i -eq $batches - 1 ) {
+            $batchSize = $rowsCount % ( $i * $batchSize)
         }
 
-        <#
-        Do {
-            $v.AddRange($logicalReader.ReadAll(100))
-            "Hello"
+
+        For ( $c = 0 ; $c -lt $meta.NumColumns ; $c++ ) { # ++$c or $c++ ? #$meta.NumColumns
+
+            $v = [System.Collections.ArrayList]@()
+
+            $columnReader = $rowGroupReader.Column( $c )
+            
+            # Return the column description
+            <#
+            
+            ColumnOrder        : Undefined
+            LogicalType        : String
+            MaxDefinitionLevel : 1
+            MaxRepetitionLevel : 0
+            Name               : first_name
+            Path               : first_name
+            SchemaNode         : ParquetSharp.Schema.PrimitiveNode
+            PhysicalType       : ByteArray
+            SortOrder          : Unsigned
+            TypeLength         : 0
+            TypePrecision      : 0
+            TypeScale          : 0
+            
+            #>
+
+            $columnDescriptor = $columnReader.ColumnDescriptor
+            "Column $( $c ) - $( $columnDescriptor.Name )"
+
+            # Only saves column names and pointers in the first batch
+            If ($i -eq 0) {
+                [void]$colNames.Add( $columnDescriptor.Name )
+                #$colMemory.Add( $c, $rowGroupReader.Column($c).LogicalReader() ) # Remember the current pointers for later continuation
+            }
+
+            # Return the metadata of a column chunk
+            <#
+            
+            Compression           : Uncompressed
+            CryptoMetadata        :
+            Encodings             : {BitPacked, PlainDictionary, Rle}
+            FileOffset            : 17317
+            IsStatsSet            : False
+            NumValues             : 1000
+            TotalCompressedSize   : 2988
+            TotalUncompressedSize : 2988
+            Statistics            :
+            Type                  : ByteArray
+            
+            #>
+
+            #$colChunkMetaData = $rowGroupReader.MetaData.GetColumnChunkMetaData($c)
+            [void]$columnReader.Skip( $i * $batchSize ) # skip x rows
+            #$columnReader.LogicalReader().BufferLength
+            #$columnReader.LogicalReader().LogicalType
             #$columnReader.LogicalReader().HasNext
-        } until ( $logicalReader.HasNext -eq $false )
-        #>
+            $logicalReader = $columnReader.LogicalReader()
+
+            # Read the data in batches
+            # Call always the next n rows            
+            #[void]$v.AddRange( $colMemory.$c.ReadAll($batchSize) )
+            [void]$v.AddRange($columnReader.LogicalReader().ReadAll($batchSize))
+            $logicalReader.ReadBatch()
+
+            <#
+            Do {
+                $v.AddRange($logicalReader.ReadAll(100))
+                "Hello"
+                #$columnReader.LogicalReader().HasNext
+            } until ( $logicalReader.HasNext -eq $false )
+            #>
+
+            [void]$colValues.Add($v)
+
+        }
+
+        # Convert to table / or put into database after one batch
+        $table = [System.Collections.ArrayList]@()
+        For ( $y = 0 ; $y -lt $batchSize ; $y++ ) {
+            $row = [PSCustomObject]@{}
+            For ( $x = 0 ; $x -lt $meta.NumColumns ; $x++ ) {
+                $row | Add-Member -MemberType NoteProperty -Name $colNames[$x] -Value $colValues[$x][$y]
+            }
+            [void]$table.Add($row)
+        }
+        $table | Out-GridView
+
     }
 
 }
